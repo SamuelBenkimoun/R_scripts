bec8b71d2f9a5e9f9905f7e621dd028f87c7d630
SamuelBenkimoun <samuel.benkimoun@ensg.eu>
2019-07-19 11:24:56 +0530

    R script to rename and join a list of csv files from FB data

---
+++ rrequestmergingcsv.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(purrr)
+# setting working directory where the csv data is
+setwd("./GIS Data/Northern-India-Nepal-And-Pakistan-Disease-Prevention-Map-May-29-2019 (1)/northern_india_nepal_and_pakistan_disease_prevention_map_may_29_2019_facebook_population_administrative_regions")
+# store the content of the whole folder
+list_csv <- list.files()
+# read the csv data
+data_csv <- lapply(list_csv, read.csv, header=TRUE, sep=",")
+# keeping only the spatial id and the population data of each file
+data_csv <- lapply(data_csv, subset, select=c(Spaco.Id, Crisis..People))
+# for the population rename the column by filename [date + hour] for further joint
+for (i in 1:length(data_csv)){colnames(data_csv[[i]])[2] <- substr(list_csv[i], 1, nchar(list_csv[i]) - 4)}
+# applying a left join by spatial id to have every population data by time step for every spatial entity
+data_csv <- data_csv %>% reduce(left_join, by = "Spaco.Id")
+# writing the resulting csv file
+write.table(data_csv, ".", sep=";")
+
+
+
+
