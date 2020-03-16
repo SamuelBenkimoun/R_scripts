36edea2ff336a1e6bd00ae9fb59dbf38825bb2c5
SamuelBenkimoun <22637957+SamuelBenkimoun@users.noreply.github.com>
2020-03-16 13:13:22 +0530

    Update tiles_aggregation.R

--- tiles_aggregation.R
+++ tiles_aggregation.R
@@ -10,9 +10,10 @@ data_csv2 <- lapply(data_csv, subset, select=c(Geometry, Crisis..People.Moving))
 # keeping only the tiles id and region name to be joined later (on the basis of geometry) to label the recordings
 data_csv <- lapply(data_csv, subset, select=c(Geometry, Starting.Region.Name, Ending.Region.Name, Starting.Location, Ending.Location))
 # for the population rename the column by filename [date + hour] for further joint
-for (i in 1:length(data_csv)){colnames(data_csv[[i]])[4] <- substr(list_csv[i], 1, nchar(list_csv[i]) - 4)}
-# for (i in 1:length(data_csv)){colnames(data_csv[[i]])[4] <- as.character(data_csv[i]$Date.Time[[1]])} 
+for (i in 1:length(data_csv2)){colnames(data_csv2[[i]])[4] <- substr(list_csv[i], 1, nchar(list_csv[i]) - 4)}
+# for (i in 1:length(data_csv2)){colnames(data_csv2[[i]])[3] <- as.character(data_csv2[[i]]$Date.Time[[1]])} 
 # applying a left join by geometry to have every population data by time step for every spatial entity
+# for (i in 1:length(data_csv2)){data_csv2[[i]]$Date.Time <- NULL}
 data_csv <- data_csv %>% reduce(left_join, by = "Geometry")
 # applying a left join by geometry to have all the labels ready
 data_csv2 <- data_csv2 %>% reduce(left_join, by = "Geometry")
@@ -20,6 +21,7 @@ data_csv2 <- data_csv2 %>% reduce(left_join, by = "Geometry")
 data_csv2 <- subset(data_csv2, select = c(Geometry, Starting.Region.Name.x, Ending.Region.Name.x, Starting.Location.x, Ending.Location.x))
 # joining the countings and the region names
 data_csv2 <- left_join(data_csv2, data_csv, by= c("Geometry"="Geometry"))
+data_csv2[is.na(data_csv2)] = 0
 # writing the resulting csv file
 write.csv(data_csv2, file="../mobilites_tiles_joined.csv")
 # rewriting a file without intern mobilities
