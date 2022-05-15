02de94170783b7d67df266b295126d89fca95a87
SamuelBenkimoun <22637957+SamuelBenkimoun@users.noreply.github.com>
2022-05-15 18:45:23 +0530

    Update dengue_sp_analysis.R
    Changing the imported file, only averaging weekdays vs weekends

--- dengue_sp_analysis.R
+++ dengue_sp_analysis.R
@@ -8,13 +8,44 @@ library(readr)
 library(tidyr)
 options(scipen=999)
 setwd("../northern_india_nepal_and_pakistan_disease_prevention_map_may_29_2019_movement_between_administrative_regions/")
-#Importing and reading the movement data
-list_csv <- list.files()
-data_csv <- lapply(list_csv, read.csv, header=TRUE, sep=",")
-#Keeping only the fields of interest
-data_csv <- lapply(data_csv, subset, select=c(Starting.Location, Ending.Location, Starting.Region.Name, Ending.Region.Name, Crisis..People.Moving, Geometry))
-#Looping to merge all the files into one, keeping the location information, and joining the movement values for the different timesteps to it.
-i=1
-for (i in 1:length(data_csv)){colnames(data_csv[[i]])[5] <- substr(list_csv[i], 1, nchar(list_csv[i]) - 4)}
-data_csv <- data_csv %>% reduce(left_join, by = c("Starting.Location", "Ending.Location"))
-data_csv <- cbind(data_csv[1:4],data_csv[6],data_csv[5:ncol(data_csv)] %>% select(-contains(c("Region", "Location", "Geometry"))))
+#Importing the movement data (already agregated in one file)
+dt <- read_delim("../Delhi Tiles Til 26 March 2020/Delhi_Tiles_till_2603.csv", ";", trim_ws = TRUE)
+#Keeping the dates before the lockdown implementation to get an averaged value week day vs week-end
+dt <- dt[2:45]
+colnames(dt)[6:ncol(dt)] <- paste(
+  weekdays(
+    parse_date_time(
+      colnames(dt[6:ncol(dt)]),
+      orders="ymd HM")
+    ),".",
+  c("5h30am","1h30pm","9h30pm"), 
+  sep = "")
+#Averaging the number of people moving by timestep, week-days (wd) and week-end (we) separately
+mean <- dt[grep("5h30",colnames(dt))]
+mean <- mean[-(grep("samedi|dimanche",colnames(mean)))]
+dt$wd.5h30am <- round(rowMeans(mean),0)
+
+mean <- dt[grep("1h30",colnames(dt))]
+mean <- mean[-(grep("samedi|dimanche",colnames(mean)))]
+dt$wd.1h30pm <- round(rowMeans(mean),0)
+
+mean <- dt[grep("9h30",colnames(dt))]
+mean <- mean[-(grep("samedi|dimanche",colnames(mean)))]
+dt$wd.9h30pm <- round(rowMeans(mean),0)
+
+mean <- dt[grep("5h30",colnames(dt))]
+mean <- mean[grep("samedi|dimanche",colnames(mean))]
+dt$we.5h30am <- round(rowMeans(mean),0)
+
+mean <- dt[grep("1h30",colnames(dt))]
+mean <- mean[grep("samedi|dimanche",colnames(mean))]
+dt$we.1h30pm <- round(rowMeans(mean),0)
+
+mean <- dt[grep("9h30",colnames(dt))]
+mean <- mean[grep("samedi|dimanche",colnames(mean))]
+dt$we.9h30pm <- round(rowMeans(mean),0)
+
+#Keeping only the averaged values for weekdays and weekends
+dt <- dt[-(6:44)]
+#Writing the output df in a csv file for safety !
+write.csv(dt, "./delhi_tiles_avg_28febto8march.csv")
